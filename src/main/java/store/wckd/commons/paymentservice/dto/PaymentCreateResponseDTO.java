e6cd714151b5ee85fc1cc13c1fd4e1f08f4d7bc9
devwckd <dev.wckd@gmail.com>
2020-08-09 20:31:19 -0300

    Created PaymentCreateResponseDTO.

---
+++ src/main/java/store/wckd/commons/paymentservice/dto/PaymentCreateResponseDTO.java
@@ -0,0 +1,44 @@
+package store.wckd.commons.paymentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import store.wckd.commons.paymentservice.enumeration.AutoReturn;
+import store.wckd.commons.paymentservice.enumeration.PaymentGateway;
+import store.wckd.commons.paymentservice.model.Item;
+import store.wckd.commons.paymentservice.model.Payer;
+import store.wckd.commons.paymentservice.model.RedirectUrls;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentCreateResponseDTO implements Serializable {
+
+    @NotNull
+    private PaymentGateway paymentGateway;
+
+    @NotNull
+    @Valid
+    private Item[] items;
+
+    @NotNull
+    @Valid
+    private Payer payer;
+
+    @NotNull
+    @Valid
+    private RedirectUrls redirectUrls;
+
+    @NotNull
+    private AutoReturn autoReturn = AutoReturn.APPROVED;
+
+    @NotNull
+    @Size(max = 500)
+    private String notificationUrl = "";
+
+}
