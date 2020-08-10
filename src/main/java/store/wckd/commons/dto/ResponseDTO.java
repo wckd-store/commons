e393e32c3f9ab6eae5d7b9a44fc0d095dd0f4587
devwckd <dev.wckd@gmail.com>
2020-08-10 00:49:20 -0300

    Fixed ResponseDTO content.

--- src/main/java/store/wckd/commons/dto/ResponseDTO.java
+++ src/main/java/store/wckd/commons/dto/ResponseDTO.java
@@ -1,10 +1,24 @@
 package store.wckd.commons.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import store.wckd.commons.enumeration.ResposeStatus;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseDTO<T> {
 
     private ResposeStatus status;
     private T data;
+
+    public static <T> ResponseDTO<T> ok(T data) {
+        return new ResponseDTO<>(ResposeStatus.OK, data);
+    }
+
+    public static <T> ResponseDTO<T> error(T data) {
+        return new ResponseDTO<>(ResposeStatus.ERROR, data);
+    }
 
 }
