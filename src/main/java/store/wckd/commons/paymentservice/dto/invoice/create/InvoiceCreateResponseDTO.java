c1caa1018f7f6c7d7a7aebe539f29bbf67e16f35
devwckd <dev.wckd@gmail.com>
2020-08-09 21:45:11 -0300

    Fixed InvoiceCreateResponseDTO content.

--- src/main/java/store/wckd/commons/paymentservice/dto/invoice/create/InvoiceCreateResponseDTO.java
+++ src/main/java/store/wckd/commons/paymentservice/dto/invoice/create/InvoiceCreateResponseDTO.java
@@ -3,15 +3,7 @@ package store.wckd.commons.paymentservice.dto.invoice.create;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import store.wckd.commons.paymentservice.enumeration.AutoReturn;
-import store.wckd.commons.paymentservice.enumeration.PaymentGateway;
-import store.wckd.commons.paymentservice.model.Item;
-import store.wckd.commons.paymentservice.model.Payer;
-import store.wckd.commons.paymentservice.model.RedirectUrls;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data
@@ -19,26 +11,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class InvoiceCreateResponseDTO implements Serializable {
 
-    @NotNull
-    private PaymentGateway paymentGateway;
-
-    @NotNull
-    @Valid
-    private Item[] items;
-
-    @NotNull
-    @Valid
-    private Payer payer;
-
-    @NotNull
-    @Valid
-    private RedirectUrls redirectUrls;
-
-    @NotNull
-    private AutoReturn autoReturn = AutoReturn.APPROVED;
-
-    @NotNull
-    @Size(max = 500)
-    private String notificationUrl = "";
+    private String invoiceUrl;
+    private String invoiceSandboxUrl;
 
 }
